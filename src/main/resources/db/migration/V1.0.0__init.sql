ca360d3ddeff270b2fcf0e52ce304318dcbc4ead
Su <mingjinger@gmail.com>
2021-06-25 11:54:29 +0800

    delete flyway

--- src/main/resources/db/migration/V1.0.0__init.sql
+++
@@ -1,7 +0,0 @@
--- test
-CREATE TABLE `test` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) DEFAULT NULL,
-  `desc` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

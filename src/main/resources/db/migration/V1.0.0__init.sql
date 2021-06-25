57007743a0e0d6d546e6e75b7c20f101357b6afa
Su <mingjinger@gmail.com>
2021-06-25 10:46:30 +0800

    mysql flyway

---
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -0,0 +1,7 @@
+-- test
+CREATE TABLE `test` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
